5df932e07526c5d807e7c95d687508dc31152d37
csetraynor <c.traynor@warwick.ac.uk>
2018-06-04 17:31:57 +0100

    add curated dataset for metaanalysis test

---
+++ R/extract_std_error.R
@@ -0,0 +1,26 @@
+#' Extract standard error
+#'
+#' Estimate the standard error
+#'
+#' @param level It's the level of the ci which is used for finding the quantile function from a t-distribution with df degrees of freedom. Default is CI 95% which correspond to perc .975 (Others ci may be used).  The quantile will be differently obtained if p_value is provided.
+#' @param n number of patients
+#' @param upper_limit upper limit of confidence interval
+#' @param lower_limit lower limit of confidence interval
+#' @param p_value It is assumed that p is equal to the upper bound, difference is required.
+#' @param diff The difference
+#' @return SE_diff
+#' @keywords se
+#' @author Carlos S Traynor
+#' @export est_se
+est_se <- function(n, level = .95 , ul, ll, diff, p = NA){
+  df <- n -2   # This is  -2  because assumes that we are estimating two values one in treatment group and one in control group
+  if(is.na(p)){
+    perc <- 1-(1-level)/2 #Find percentile
+    t_alpha <- qt(perc, df)  # Find the value of the t statistic for the CI
+    se = (ul - ll) / (2 * t_alpha)
+  } else{
+    t_alpha <- qt(1- p/2, df)
+    se = diff / t_alpha
+  }
+  return(se)
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # condma
-condma
+Conditional Meta-Analysis embraces the Bayesian paradigm to perform reliable meta-analysis and takes advatage of the HMC techniques available from Stan.
